d3f1aeddf6bf138465d37e969767482aaf92afb9
florisusuri <florisusuri23@gmail.com>
2026-02-09 14:38:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function showGoal() {
+document.getElementById("goalText").innerText =
+"Të ofrojmë shërbime dhe produkte cilësore, profesionale dhe të besueshme, duke plotësuar nevojat e klientëve tanë me korrektësi dhe përkushtim"}
